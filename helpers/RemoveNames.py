eca811b7afe4f1713ddbf916f428a49517aad08b
gustavbagger <gustavbagger@me.com>
2025-11-13 12:31:37 +1100

    prefix_tree and RemoveNames helpers

---
+++ helpers/RemoveNames.py
@@ -0,0 +1,11 @@
+def RemoveNames(filepath):
+    file = open(filepath,"r")
+    file_string = file.read()
+    words = file_string.split("\n")
+    culled_words = list()
+    for word in words:
+        if word != "" and not word[0].isupper():
+            culled_words.append(word)
+    
+    output_file = open(filepath[:-4]+"_culled.txt","w")
+    output_file.write("\n".join(culled_words))
---
+++ main.py
@@ -0,0 +1,3 @@
+from helpers.RemoveNames import RemoveNames
+
+RemoveNames("./six_letter_words.txt")
